cdac553c1600b7d206f1829434fbc7abd2942ba6
Maria sophia Mascardo <mariasophiamascardo@Pias-MacBook-Air.local>
2025-04-08 21:42:52 +0200

    doneee

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import RoomDetails from "./pages/RoomDetails";
 import NotFound from './pages/NotFound'
 import SignUp from './pages/SignUp'
 import LogIn from './pages/LogIn'
-import DatePicker from "./pages/DatePicker"
+import Booking from "./pages/Booking"
 import AccPage from './pages/AccPage'
 import "./App.css"
 
@@ -22,7 +22,7 @@ function App() {
     <Route path="*" element={<NotFound />} />
     <Route path="/signup" element={<SignUp />} />
     <Route path="/login" element={<LogIn />} />
-    <Route path="/booking/:roomId/:userId" element={<DatePicker />} />
+    <Route path="/booking/:roomId/:userId" element={<Booking />} />
     <Route path="/accounts/:userId" element={<AccPage />} />
   </Routes>
    
